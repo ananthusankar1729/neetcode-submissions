6c4c190a409389e19914bd739f9d1cf9f32d1ee8
ananthusankar1729 <211710819+ananthusankar1729@users.noreply.github.com>
2026-05-27 13:42:40 +0530

    Add: valid-palindrome-ii - submission-8

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        # #if palindrome 
+        # l, r = 0, len(s)-1
+        # while l<r:
+        #     if s[l]!=s[r]:
+        #         skipL, skipR = s[l+1:r+1], s[l:r]
+        #         return (skipL==skipL[::-1] or skipR==skipR[::-1])
+        #     l+=1
+        #     r-=1
+        # return True    
+        def isPalindrome(s):
+            if s == s[::-1]:
+                return True
+            return False
+        l, r = 0, len(s)-1
+        while l<r :
+            if s[l]!=s[r]:
+                return (isPalindrome(s[l+1:r+1]) or isPalindrome(s[l:r]))
+            l+=1
+            r-=1
+        return True   
+
+        
